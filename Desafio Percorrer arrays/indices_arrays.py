f918313bac4f1991f4c2ac007fe81b497672633c
Paulo Ricardo <66179718+Meursault92@users.noreply.github.com>
2023-11-20 18:40:05 -0300

    Commit do desafio

---
+++ Desafio Percorrer arrays/indices_arrays.py
@@ -0,0 +1,17 @@
+# Copie o array apresentado embaixo no seu editor de código, e imprima no terminal: a quantidade de elementos que ele possui, 
+# o dado salvo no índice 2, o dado salvo no índice 9, e dado salvo no índice 14.
+# lista_musicos = [ 'Djavan', 'Roberto Carlos', 'Elis Regina', 'Tom Jobim', 'Milton Nascimento', 'Chico Buarque', 'Nara Leão',
+# 'Pitty', 'Simonal', 'Moacir Santos', 'Caetano Veloso', 'Elza Soares', 'Paulinho da Viola', 'Yamandú Costa', 'Gal Costa'] 
+
+lista_musicos = [ 'Djavan', 'Roberto Carlos', 'Elis Regina', 'Tom Jobim', 'Milton Nascimento', 'Chico Buarque', 'Nara Leão', 'Pitty', 'Simonal', 'Moacir Santos', 'Caetano Veloso', 'Elza Soares', 'Paulinho da Viola', 'Yamandú Costa', 'Gal Costa']
+
+quantidade_musicos = len(lista_musicos)
+
+musico_indice_2 = lista_musicos[2]
+musico_indice_9 = lista_musicos[9]
+musico_indice_14 = lista_musicos[14]
+
+print(f"A quantidade de músicos é: {quantidade_musicos}")
+print(f"O músico do índice 2 é: {musico_indice_2}")
+print(f"O músico do índice 9 é: {musico_indice_9}")
+print(f"O músico do índice 14 é: {musico_indice_14}")
